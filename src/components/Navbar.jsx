51566471b2c4b0ddd1302549ca2baee3a8ed84b3
Greg Richardson <gsr142@gmail.com>
2024-04-05 12:57:14 -0700

    added navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+// import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+
+export default function ButtonAppBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Button href="/about" color="inherit">
+            About
+          </Button>
+          <Button href="/portfolio" color="inherit">
+            Portfolio
+          </Button>
+          <Button href="/contact" color="inherit">
+            Contact
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
