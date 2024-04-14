8e390c70af31e4d4606209b239a0271a88a807e4
Greg Richardson <gsr142@gmail.com>
2024-04-14 15:06:16 -0700

    add auto margin to navbar buttons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,30 +3,19 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
 
 export default function ButtonAppBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Button href="/" color="inherit">
+        <Toolbar justifyContent="center">
+          <Button href="/" color="inherit" sx={{ margin:"auto" }}>
             <Typography variant="h6">About</Typography>
           </Button>
-          <Button href="/portfolio" color="inherit">
+          <Button href="/portfolio" color="inherit"sx={{ margin:"auto" }}>
           <Typography variant="h6">Portfolio</Typography>
           </Button>
-          <Button href="/contact" color="inherit">
+          <Button href="/contact" color="inherit" sx={{ margin:"auto" }}>
           <Typography variant="h6">Contact</Typography>
           </Button>
         </Toolbar>
